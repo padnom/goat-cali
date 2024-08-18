e56cf6e3e32154117d33cf3f29c0e1d613ee0280
ROELLINGER <jonathan.roellinger@arteliagroup.com>
2024-08-18 15:48:29 +0200

    refactor: tests to use Theory and InlineData attributes
    Refactored test methods in FellowshipOfTheRingServiceTests to use
    [Theory] and [InlineData] attributes for parameterized testing.
    Updated various test methods to accept parameters for character
    details, regions, and expected outcomes, enhancing flexibility
    and test coverage. Replaced several [Fact] attributes with
    [Theory] and [InlineData].

--- LordOfTheRings.Tests/FellowshipOfTheRingServiceTest.cs
+++ LordOfTheRings.Tests/FellowshipOfTheRingServiceTest.cs
@@ -3,26 +3,32 @@
 namespace LordOfTheRings.Tests;
 public class FellowshipOfTheRingServiceTests
 {
-    [Fact]
-    public void AddMember_CharacterWithDuplicateName_ThrowsInvalidOperationException()
+    [Theory]
+    [InlineData("Frodo", "Hobbit", "Sting", 10, "Shire")]
+    public void AddMember_CharacterWithDuplicateName_ThrowsInvalidOperationException(
+        string name,
+        string race,
+        string weaponName,
+        int damage,
+        string region)
     {
         // Arrange
         var service = new FellowshipOfTheRingService();
 
         var character1 = new Character
                          {
-                             N = "Frodo",
-                             R = "Hobbit",
-                             W = new Weapon { Name = "Sting", Damage = 10, },
-                             C = "Shire",
+                             N = name,
+                             R = race,
+                             W = new Weapon { Name = weaponName, Damage = damage, },
+                             C = region,
                          };
 
         var character2 = new Character
                          {
-                             N = "Frodo",
-                             R = "Hobbit",
-                             W = new Weapon { Name = "Sting", Damage = 10, },
-                             C = "Shire",
+                             N = name,
+                             R = race,
+                             W = new Weapon { Name = weaponName, Damage = damage, },
+                             C = region,
                          };
 
         service.AddMember(character1);
@@ -41,7 +47,12 @@ public class FellowshipOfTheRingServiceTests
     [InlineData("Frodo", "Hobbit", null, 10, "Character must have a weapon.")]
     [InlineData("Frodo", "Hobbit", "", 10, "A weapon must have a name.")]
     [InlineData("Frodo", "Hobbit", "Sting", 0, "A weapon must have a damage level.")]
-    public void AddMember_InvalidCharacter_ThrowsArgumentException(string name, string race, string weaponName, int damage, string expectedMessage)
+    public void AddMember_InvalidCharacter_ThrowsArgumentException(
+        string name,
+        string race,
+        string weaponName,
+        int damage,
+        string expectedMessage)
     {
         // Arrange
         var service = new FellowshipOfTheRingService();
@@ -73,162 +84,181 @@ public class FellowshipOfTheRingServiceTests
         act.Should().Throw<ArgumentNullException>();
     }
 
-    [Fact]
-    public void AddMember_ValidCharacter_AddsCharacter()
+    [Theory]
+    [InlineData("Frodo", "Hobbit", "Sting", 10, "Shire", "Frodo (Hobbit) with Sting in Shire")]
+    [InlineData("Aragorn", "Human", "Sword", 100, "Rivendell", "Aragorn (Human) with Sword in Rivendell")]
+    public void AddMember_ValidCharacter_AddsCharacter(
+        string name,
+        string race,
+        string weaponName,
+        int damage,
+        string region,
+        string expectedOutput)
     {
         // Arrange
         var service = new FellowshipOfTheRingService();
 
         var character = new Character
                         {
-                            N = "Frodo",
-                            R = "Hobbit",
-                            W = new Weapon { Name = "Sting", Damage = 10, },
-                            C = "Shire",
+                            N = name,
+                            R = race,
+                            W = new Weapon { Name = weaponName, Damage = damage, },
+                            C = region,
                         };
 
         // Act
         service.AddMember(character);
 
         // Assert
-        var expectedOutput = "Frodo (Hobbit) with Sting in Shire";
         service.ToString().Should().Contain(expectedOutput);
     }
 
-    [Fact]
-    public void MoveMembersToRegion_MovingFromMordor_ThrowsException()
+    [Theory]
+    [InlineData("Frodo", "Mordor", "Shire", "Cannot move Frodo from Mordor to Shire. Reason: There is no coming back from Mordor.")]
+    public void MoveMembersToRegion_MovingFromMordor_ThrowsException(
+        string name,
+        string fromRegion,
+        string toRegion,
+        string expectedMessage)
     {
         // Arrange
         var service = new FellowshipOfTheRingService();
-        service.AddMember(new Character { N = "Frodo", R = "Hobbit", W = new Weapon { Name = "Sting", Damage = 10, }, C = "Mordor", });
+        service.AddMember(new Character { N = name, R = "Hobbit", W = new Weapon { Name = "Sting", Damage = 10, }, C = fromRegion, });
 
-        var memberNames = new List<string> { "Frodo", };
+        var memberNames = new List<string> { name, };
 
         // Act
-        var act = () => service.MoveMembersToRegion(memberNames, "Shire");
+        var act = () => service.MoveMembersToRegion(memberNames, toRegion);
 
         // Assert
-        act.Should()
-           .Throw<InvalidOperationException>()
-           .WithMessage("Cannot move Frodo from Mordor to Shire. Reason: There is no coming back from Mordor.");
+        act.Should().Throw<InvalidOperationException>().WithMessage(expectedMessage);
     }
 
-    [Fact]
-    public void MoveMembersToRegion_ValidMembers_MoveToNewRegion()
+    [Theory]
+    [InlineData("Aragorn", "Human", "Sword", 100, "Rivendell", "Gondor", "Aragorn (Human) with Sword in Gondor")]
+    [InlineData("Legolas", "Elf", "Bow", 50, "Rivendell", "Gondor", "Legolas (Elf) with Bow in Gondor")]
+    public void MoveMembersToRegion_ValidMembers_MoveToNewRegion(
+        string name,
+        string race,
+        string weaponName,
+        int damage,
+        string fromRegion,
+        string toRegion,
+        string expectedOutput)
+    {
+        // Arrange
+        var service = new FellowshipOfTheRingService();
+        service.AddMember(new Character { N = name, R = race, W = new Weapon { Name = weaponName, Damage = damage, }, C = fromRegion, });
+
+        var memberNames = new List<string> { name, };
+
+        // Act
+        service.MoveMembersToRegion(memberNames, toRegion);
+
+        // Assert
+        service.ToString().Should().Contain(expectedOutput);
+    }
+
+    [Theory]
+    [InlineData("Gollum", "Hobbit", "Dagger", 5, "Shire", "Mordor", "Gollum (Hobbit) with Dagger in Mordor")]
+    public void MoveMembersToRegion_ValidMoveToMordor_ShowsCorrectOutput(
+        string name,
+        string race,
+        string weaponName,
+        int damage,
+        string fromRegion,
+        string toRegion,
+        string expectedOutput)
+    {
+        // Arrange
+        var service = new FellowshipOfTheRingService();
+        service.AddMember(new Character { N = name, R = race, W = new Weapon { Name = weaponName, Damage = damage, }, C = fromRegion, });
+
+        var memberNames = new List<string> { name, };
+
+        // Act
+        service.MoveMembersToRegion(memberNames, toRegion);
+
+        // Assert
+        service.ToString().Should().Contain(expectedOutput);
+    }
+
+    [Theory]
+    [InlineData("Rivendell", true)]
+    [InlineData("Mordor", false)]
+    public void PrintMembersInRegion_RegionHasMembers_PrintsCorrectly(string region, bool hasMembers)
     {
         // Arrange
         var service = new FellowshipOfTheRingService();
         service.AddMember(new Character { N = "Aragorn", R = "Human", W = new Weapon { Name = "Sword", Damage = 100, }, C = "Rivendell", });
         service.AddMember(new Character { N = "Legolas", R = "Elf", W = new Weapon { Name = "Bow", Damage = 50, }, C = "Rivendell", });
 
-        var memberNames = new List<string> { "Aragorn", "Legolas", };
-
         // Act
-        service.MoveMembersToRegion(memberNames, "Gondor");
-
-        // Assert
-        var expectedOutput = "Aragorn (Human) with Sword in Gondor";
-        service.ToString().Should().Contain(expectedOutput);
-        service.ToString().Should().Contain("Legolas (Elf) with Bow in Gondor");
-    }
-
-    [Fact]
-    public void MoveMembersToRegion_ValidMoveToMordor_ShowsCorrectOutput()
-    {
-        // Arrange
-        var service = new FellowshipOfTheRingService();
-        service.AddMember(new Character { N = "Gollum", R = "Hobbit", W = new Weapon { Name = "Dagger", Damage = 5, }, C = "Shire", });
-
-        var memberNames = new List<string> { "Gollum", };
-
-        // Act
-        service.MoveMembersToRegion(memberNames, "Mordor");
-
-        // Assert
-        service.ToString().Should().Contain("Gollum (Hobbit) with Dagger in Mordor");
-    }
-
-    [Fact]
-    public void PrintMembersInRegion_RegionHasMembers_PrintsCorrectly()
-    {
-        // Arrange
-        var service = new FellowshipOfTheRingService();
-        service.AddMember(new Character { N = "Aragorn", R = "Human", W = new Weapon { Name = "Sword", Damage = 100, }, C = "Rivendell", });
-        service.AddMember(new Character { N = "Legolas", R = "Elf", W = new Weapon { Name = "Bow", Damage = 50, }, C = "Rivendell", });
-
-        // Act
-        var act = () => service.PrintMembersInRegion("Rivendell");
+        var act = () => service.PrintMembersInRegion(region);
 
         // Assert
         act.Should().NotThrow(); // Ensure it doesn't throw an exception
-        // For console output, you'd typically mock the Console, but here we just ensure no exceptions are thrown
     }
 
-    [Fact]
-    public void PrintMembersInRegion_RegionHasNoMembers_PrintsNoMembers()
+    [Theory]
+    [InlineData("Gimli", "No character with the name 'Gimli' exists in the fellowship.")]
+    public void RemoveMember_NonExistentMember_ThrowsException(string name, string expectedMessage)
     {
         // Arrange
         var service = new FellowshipOfTheRingService();
-        service.AddMember(new Character { N = "Aragorn", R = "Human", W = new Weapon { Name = "Sword", Damage = 100, }, C = "Rivendell", });
 
         // Act
-        var act = () => service.PrintMembersInRegion("Mordor");
+        var act = () => service.RemoveMember(name);
+
+        // Assert
+        act.Should().Throw<InvalidOperationException>().WithMessage(expectedMessage);
+    }
+
+    [Theory]
+    [InlineData("Boromir", "Human", "Sword", 90, "Gondor")]
+    public void RemoveMember_ValidMember_RemovesMember(
+        string name,
+        string race,
+        string weaponName,
+        int damage,
+        string region)
+    {
+        // Arrange
+        var service = new FellowshipOfTheRingService();
+        service.AddMember(new Character { N = name, R = race, W = new Weapon { Name = weaponName, Damage = damage, }, C = region, });
+
+        // Act
+        service.RemoveMember(name);
+
+        // Assert
+        service.ToString().Should().NotContain(name);
+    }
+
+    [Theory]
+    [InlineData("Gimli", "Axe", 80)]
+    public void UpdateCharacterWeapon_CharacterDoesNotExist_DoesNotThrow(string name, string newWeaponName, int newDamage)
+    {
+        // Arrange
+        var service = new FellowshipOfTheRingService();
+
+        // Act
+        var act = () => service.UpdateCharacterWeapon(name, newWeaponName, newDamage);
 
         // Assert
         act.Should().NotThrow();
+        service.ToString().Should().NotContain(newWeaponName);
     }
 
-    [Fact]
-    public void RemoveMember_NonExistentMember_ThrowsException()
-    {
-        // Arrange
-        var service = new FellowshipOfTheRingService();
-
-        // Act
-        var act = () => service.RemoveMember("Gimli");
-
-        // Assert
-        act.Should()
-           .Throw<InvalidOperationException>()
-           .WithMessage("No character with the name 'Gimli' exists in the fellowship.");
-    }
-
-    [Fact]
-    public void RemoveMember_ValidMember_RemovesMember()
-    {
-        // Arrange
-        var service = new FellowshipOfTheRingService();
-        service.AddMember(new Character { N = "Boromir", R = "Human", W = new Weapon { Name = "Sword", Damage = 90, }, C = "Gondor", });
-
-        // Act
-        service.RemoveMember("Boromir");
-
-        // Assert
-        service.ToString().Should().NotContain("Boromir");
-    }
-
-    [Fact]
-    public void UpdateCharacterWeapon_CharacterDoesNotExist_DoesNotThrow()
-    {
-        // Arrange
-        var service = new FellowshipOfTheRingService();
-
-        // Act
-        var act = () => service.UpdateCharacterWeapon("Gimli", "Axe", 80);
-
-        // Assert
-        act.Should().NotThrow(); // The method should not throw an exception if the character doesn't exist
-    }
-
-    [Fact]
-    public void UpdateCharacterWeapon_CharacterExists_WeaponUpdatedCorrectly()
+    [Theory]
+    [InlineData("Legolas", "Longbow", 75)]
+    public void UpdateCharacterWeapon_CharacterExists_WeaponUpdatedCorrectly(string name, string newWeaponName, int newDamage)
     {
         // Arrange
         var service = new FellowshipOfTheRingService();
 
         var character = new Character
                         {
-                            N = "Legolas",
+                            N = name,
                             R = "Elf",
                             W = new Weapon { Name = "Bow", Damage = 50, },
                             C = "Rivendell",
@@ -237,34 +267,12 @@ public class FellowshipOfTheRingServiceTests
         service.AddMember(character);
 
         // Act
-        service.UpdateCharacterWeapon("Legolas", "Longbow", 75);
+        var act = () => service.UpdateCharacterWeapon(name, newWeaponName, newDamage);
 
         // Assert
-        character.W.Name.Should().Be("Longbow");
-        character.W.Damage.Should().Be(75);
-    }
+        act.Should().NotThrow();
 
-    [Fact]
-    public void UpdateCharacterWeapon_ValidCharacter_UpdatesWeapon()
-    {
-        // Arrange
-        var service = new FellowshipOfTheRingService();
-
-        var character = new Character
-                        {
-                            N = "Aragorn",
-                            R = "Human",
-                            W = new Weapon { Name = "Sword", Damage = 100, },
-                            C = "Rivendell",
-                        };
-
-        service.AddMember(character);
-
-        // Act
-        service.UpdateCharacterWeapon("Aragorn", "Andúril", 150);
-
-        // Assert
-        var expectedOutput = "Aragorn (Human) with Andúril in Rivendell";
+        var expectedOutput = $"{name} (Elf) with {newWeaponName} in Rivendell";
         service.ToString().Should().Contain(expectedOutput);
     }
 }
